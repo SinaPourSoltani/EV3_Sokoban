c8667dc4dff90042b7589638b1dbdd9033a00739
Sina Pour Soltani <sinapsoltani@gmail.com>
2020-11-01 10:30:04 +0100

    Change methods and parameters after test on track.

--- Python/EV3.py
+++ Python/EV3.py
@@ -5,18 +5,21 @@ from time import sleep
 
 # defines
 STOP_SPEED = 0
-CRUISE_SPEED = 60
-ADJUST_SPEED = CRUISE_SPEED * 1.2
+CRUISE_SPEED = 40
+ADJUST_SPEED = CRUISE_SPEED * 1.4
 TURN_SPEED = 30
 
 END = 'X'
 
-TURN_TIME = 1.1
+# delays
+DELAY_LINE_DETECTED = 0.25
+DELAY_PRE_TURN = 0.1
+DELAY_POST_TURN = 0.2
 
 # colors
 BLACK = 1
 WHITE = 0
-COLOR_THRESHOLD = 30
+COLOR_THRESHOLD = 10
 
 # directions
 FORWARD = 1
@@ -45,6 +48,8 @@ INDEX2ORI = {
     RIGHT:  'RIGHT',
     DOWN:   'DOWN',
 }
+
+# TODO Use wheel rotations instead of speed for rotations so that speeds can be increased invariant of delay times
 
 class EV3:
     def __init__(self, start_orientation, solution):
@@ -76,46 +81,50 @@ class EV3:
         self.m_left.polarity = "normal"
         self.m_right.duty_cycle_sp = STOP_SPEED
         self.m_left.duty_cycle_sp = STOP_SPEED
-        #self.m_left.ramp_up_sp = 2000
-        #self.m_right.ramp_up_sp = 5000
         self.m_left.run_direct()
         self.m_right.run_direct()
 
-    def line_follower(self):
+    def line_follower(self, dir=FORWARD):
+        '''
+        self.set_speed(self.m_left, STOP_SPEED)
+        self.set_speed(self.m_right, STOP_SPEED)
+        while True:
+            print("CSL", self.cs_left.value())
+            print("CSR", self.cs_right.value())
+        '''
         while not (self.cs_left.value() < COLOR_THRESHOLD and self.cs_right.value() < COLOR_THRESHOLD):
-            #print("CSL",self.cs_left.value())
-            #print("CSR",self.cs_right.value())
             if self.cs_left.value() < COLOR_THRESHOLD: #== BLACK:
-                self.set_speed(self.m_right,ADJUST_SPEED)
+                self.set_speed(self.m_right,ADJUST_SPEED,dir)
             elif self.cs_right.value() < COLOR_THRESHOLD: #== BLACK:
-                self.set_speed(self.m_left, ADJUST_SPEED)
+                self.set_speed(self.m_left, ADJUST_SPEED,dir)
             else:
-                self.set_speed(self.m_left, CRUISE_SPEED)
-                self.set_speed(self.m_right, CRUISE_SPEED)
+                self.set_speed(self.m_left, CRUISE_SPEED,dir)
+                self.set_speed(self.m_right, CRUISE_SPEED,dir)
 
     def check_next(self, current_move, next_move):
-        if current_move != next_move:
-            self.IS_PUSHING = False
-            self.move_backward()
-        else:
-            self.IS_PUSHING = True
+        self.IS_PUSHING = current_move == next_move
 
     def move_forward(self):
-        self.set_speed(self.m_left,CRUISE_SPEED,FORWARD)
-        self.set_speed(self.m_right,CRUISE_SPEED,FORWARD)
+        print("Moving forward.")
         # TODO use sensor to detect line of next position
         self.line_follower()
-        sleep(0.1)
+        sleep(DELAY_LINE_DETECTED)
         self.park()
 
     def move_backward(self):
-        self.set_speed(self.m_left,CRUISE_SPEED,REVERSE)
-        self.set_speed(self.m_right,CRUISE_SPEED,REVERSE)
+        print("Moving backwards.")
         # TODO use sensor to detect line of next position
-        sleep(3)
+        self.line_follower(REVERSE)
+        sleep(DELAY_LINE_DETECTED)
+        self.line_follower(REVERSE)
         self.park()
 
+    def deposit(self):
+        self.move_forward()
+        self.move_backward()
+
     def park(self):
+        print("Parking.")
         self.set_speed(self.m_right,STOP_SPEED)
         self.set_speed(self.m_left,STOP_SPEED)
         sleep(0.1)
@@ -124,39 +133,42 @@ class EV3:
         moter.duty_cycle_sp = dir * speed
 
     def turn_right(self):
+        print("Turning right.")
         self.set_speed(self.m_left,TURN_SPEED,FORWARD)
         self.set_speed(self.m_right,TURN_SPEED,REVERSE)
         # TODO maybe change to check for lines
-        while not self.cs_right.value() < COLOR_THRESHOLD:
+        sleep(DELAY_PRE_TURN)
+        while self.cs_right.value() > COLOR_THRESHOLD:
             pass
-        #sleep(TURN_TIME)
+        sleep(DELAY_POST_TURN)
         self.park()
 
     def turn_left(self):
+        print("Turning left")
         self.set_speed(self.m_left, TURN_SPEED, REVERSE)
         self.set_speed(self.m_right, TURN_SPEED, FORWARD)
         # TODO maybe change to check for lines
-        while not self.cs_left.value() < COLOR_THRESHOLD:
+        sleep(DELAY_PRE_TURN)
+        while self.cs_left.value() > COLOR_THRESHOLD:
             pass
-        #sleep(TURN_TIME)
+        sleep(DELAY_POST_TURN)
         self.park()
 
     def turn_around(self):
+        print("Turning around.")
         self.set_speed(self.m_left, TURN_SPEED, REVERSE)
         self.set_speed(self.m_right, TURN_SPEED, FORWARD)
         rotations = 0
         while rotations < 2:
             if self.cs_left.value() < COLOR_THRESHOLD:
                 rotations += 1
-                sleep(0.5)
+                sleep(DELAY_POST_TURN)
 
         self.park()
 
-        #self.turn_left()
-        #self.turn_left()
-
     def set_orientation(self, ori):
         ori_index = ORI2INDEX[ori]
+        print("Setting orientation to", INDEX2ORI[ori_index])
         turn = self.current_orientation - ori_index
 
         if turn == -1 or turn == 3:
@@ -170,16 +182,22 @@ class EV3:
 
     def execute(self, solution):
         for i, move in enumerate(solution):
-            print(INDEX2ORI[self.current_orientation], move)
+            print("NEW MOVE:",INDEX2ORI[self.current_orientation], move, "-------------------")
             if move != END:
                 self.set_orientation(move)
                 if move.islower():
                     self.move_forward()
                 else:
-                    self.move_forward() if self.IS_PUSHING else self.move_forward(); self.move_forward()
-                    self.check_next(move, solution[i+1])
+                    self.check_next(move, solution[i + 1])
+                    if self.IS_PUSHING:
+                        self.move_forward()
+                    else:
+                        self.move_forward()
+                        self.deposit()
+
             else:
                 self.park()
 
-solution = "uudd" + END
+#solution = "lllldlluRRRRRdrUUruulldRRlddllllUdrruLuulldDRddlluRRRRRdrUUruulldRurDurrdLulldddllllluuurDDldRRRRdrUUdlllluurDldRRRdrU" + END
+solution = "uRRlld"
 eve = EV3(DOWN,solution)
